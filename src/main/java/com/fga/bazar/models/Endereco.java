381467346a91bc47382b1df10eee9aa3996a3a60
Gabriel <gabriel@Cartola.localdomain>
2022-08-24 13:58:13 -0300

    correção do modelo Endereço

--- src/main/java/com/fga/bazar/models/Endereco.java
+++ src/main/java/com/fga/bazar/models/Endereco.java
@@ -5,10 +5,14 @@ import java.math.BigInteger;
 
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
+@Entity
+@Table(name="Endereco")
 public class Endereco implements Serializable{
 
     @Id
